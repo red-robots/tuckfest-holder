e1fa132a6f1394b18dbc0d3f372c6e76c583a41c
accrane <austincrane@gmail.com>
2023-10-25 16:05:39 -0400

    change homepage

--- parts/hero.php
+++ parts/hero.php
@@ -2,18 +2,21 @@
   $home_banner = get_field('home_banner');
   
   $buttons = get_field('home_buttons');
+  // echo '<pre>';
+  // print_r($buttons);
+  // echo '</pre>';
   ?>
   <?php if( $home_banner || $buttons ) { ?>
 
 
     <?php if ($home_banner) { ?>
     <div class="home-banner">
-      <div class="wrapper">
+      
         <figure>
           <img class="desktop" src="<?php echo $home_banner['url'] ?>" alt="<?php echo $home_banner['title'] ?>">
           <!-- <img class="mobile" src="<?php echo $home_banner_mobile['url'] ?>" alt="<?php echo $home_banner_mobile['title'] ?>"> -->
         </figure>
-      </div>
+      
     </div>
     <?php } ?>
 
@@ -31,17 +34,17 @@
             $link_title = ( isset($link['title']) && $link['title'] ) ? $link['title'] : '';
             $target = ( isset($link['target']) && $link['target'] ) ? $link['target'] : '_self';
             $bgcolor = ($color) ? ' style="background-color:'.$color.'"':'';
-            if ( file_exists( locate_template('parts/graphic/'.$div_id.'.php') ) ) {
-              if( $link_title &&  $pagelink ) {  ?>
-              <a href="<?php echo $pagelink ?>" target="<?php echo $target ?>" class="<?php echo $div_id?>-icon">
+            // if ( file_exists( locate_template('parts/graphic/'.$div_id.'.php') ) ) {
+            //   if( $link_title &&  $pagelink ) {  ?>
+              <a data-i="<?php echo $i; ?>" href="<?php echo $pagelink ?>" target="<?php echo $target ?>" class="<?php echo $div_id; ?>-icon">
                   <?php echo $link_title ?>
                 </a>
               <?php if ($color) { ?>
               <style type="text/css">
                 @media screen and (max-width:820px) {#<?php echo $div_id ?>.button.graphic{background-color:<?php echo $color ?>}}
               </style>  
-              <?php } ?>
-            <?php } ?>
+              <?php //} ?>
+            <?php //} ?>
             <?php } ?>
           <?php $i++; } ?>
         </div>
